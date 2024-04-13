3d7f9ce76cfc747bd23c412d5835e972d920b803
Richard D. Aureliano <89708267+Richard-D-Aureliano@users.noreply.github.com>
2024-04-13 02:27:47 -0300

    Add files via upload

---
+++ Desafio_Dragao.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 13 01:59:24 2024
+
+@author: richa
+"""
+
+C = int(input()) 
+
+for i in range (C): 
+
+    N = int(input())
+
+    if N <= 8000:
+      print("Inseto!")
+    else:
+      print("Mais de 8000!")
+      
